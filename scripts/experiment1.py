8697bd749d085cff4c9938d0d7df3a0c50d3c5c4
Aleksandra Teska <aleksandra.teska@gmail.com>
2018-04-12 12:37:20 +0200

    first working verions

--- scripts/experiment1.py
+++ scripts/experiment1.py
@@ -14,17 +14,19 @@ def update(dt):
 pyglet.clock.schedule(update)
 
 # Insert filename into WavefrontReader.
-obj_filename = rc.resources.obj_primitives
-# obj_filename = 'assets/2.obj'  # hollow cylinder
-
+# obj_filename = rc.resources.obj_primitives
+obj_filename = 'assets/2.obj'  # hollow cylinder
 obj_reader = rc.WavefrontReader(obj_filename)
 
 
 # Create Mesh
-cylinder = obj_reader.get_mesh("Cylinder", scale=.6)
+cylinder = obj_reader.get_mesh("hollow_Cylinder", scale=.6)
+cylinder.dynamic = True
 cylinder.position.xyz = 0, 0, -2
-cylinder.rotation.x = 40
+cylinder.rotation.x = 90
 cylinder.uniforms['diffuse'] = 1., 1., 1.
+
+
 
 texture = rc.Texture.from_image('assets/pict.png')
 cylinder.textures.append(texture)
@@ -38,12 +40,14 @@ shader = rc.Shader.from_file(vert = vertname, frag = fragname)
 
 # Create Scene
 scene = rc.Scene(meshes=[cylinder])
+scene.gl_states = scene.gl_states[:-1]
 
 # Create Sequence
-seq = [events.update_attribute(cylinder, 'speed', -2),
-       events.wait_duration(10.),
-       events.update_attribute(cylinder, 'speed', 2),]
-
+seq = [events.update_attribute(cylinder, 'speed', -20),
+       events.wait_duration(5.),
+       events.update_attribute(cylinder, 'speed', 20),
+       events.wait_duration(5.),
+       ]
 
 def rotate_cylinder(dt):
     global cylinder
@@ -52,7 +56,7 @@ pyglet.clock.schedule(rotate_cylinder)
 
 
 exp = events.chain_events(seq)
-# exp.next()
+next(exp)
 
 @window.event
 def on_draw():
