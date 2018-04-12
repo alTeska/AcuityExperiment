c17eae529a8d104fbf3327a27870ec7c9297c99f
Aleksandra Teska <aleksandra.teska@gmail.com>
2018-04-12 17:16:01 +0200

    sequence update

--- scripts/experiment1.py
+++ scripts/experiment1.py
@@ -1,10 +1,11 @@
 import pyglet
 import ratcave as rc
 import events
+import cfg
 
 from glob import glob
 from os import path
-
+from numpy import random
 
 # Create Window
 window = pyglet.window.Window()
@@ -14,40 +15,47 @@ def update(dt):
 pyglet.clock.schedule(update)
 
 # Insert filename into WavefrontReader.
-# obj_filename = rc.resources.obj_primitives
-obj_filename = 'assets/2.obj'  # hollow cylinder
+obj_filename = 'scripts/assets/2.obj'  # hollow cylinder
 obj_reader = rc.WavefrontReader(obj_filename)
 
 
 # Create Mesh
 cylinder = obj_reader.get_mesh("hollow_Cylinder", scale=.6)
-cylinder.dynamic = True
-cylinder.position.xyz = 0, 0, -2
-cylinder.rotation.x = 90
-cylinder.uniforms['diffuse'] = 1., 1., 1.
+cylinder.position.xyz = cfg.CYLINDER_POSITION
+cylinder.rotation.x = cfg.CYLINDER_ROTATION_X
+cylinder.uniforms.diffuse = 1., 1., 1.
 
-
-
-texture = rc.Texture.from_image('assets/pict.png')
+texture = rc.Texture.from_image(cfg.CYLINDER_TEXTURE)
 cylinder.textures.append(texture)
 cylinder.speed = 0
 
 
+# Shader
 vertname = glob(path.join(rc.resources.shader_path, 'default', '*.vert'))[0]
 fragname = glob(path.join(rc.resources.shader_path, 'default', '*.frag'))[0]
-shader = rc.Shader.from_file(vert = vertname, frag = fragname)
+shader = rc.Shader.from_file(vert=vertname, frag=fragname)
 
 
 # Create Scene
 scene = rc.Scene(meshes=[cylinder])
 scene.gl_states = scene.gl_states[:-1]
+scene.bgColor = (0., 0., 0.)
 
-# Create Sequence
-seq = [events.update_attribute(cylinder, 'speed', -20),
-       events.wait_duration(5.),
-       events.update_attribute(cylinder, 'speed', 20),
-       events.wait_duration(5.),
-       ]
+
+# Build experiment event sequence
+seq = [events.update_attribute(cylinder, 'visible', False),
+       events.wait_duration(cfg.START_TIME)]
+
+for speed in random.permutation(cfg.CYLINDER_SPEEDS * 2):
+    for direction in [1, -1]:
+        phase_seq = [
+            events.update_attribute(cylinder, 'visible', True),
+            events.update_attribute(cylinder, 'speed', speed * direction),
+            events.wait_duration(cfg.PHASE_DURATION_SECS),
+            events.update_attribute(cylinder, 'visible', False),
+            events.wait_duration(cfg.WAIT_DURATION_SECS),
+        ]
+        seq.extend(phase_seq)
 
 def rotate_cylinder(dt):
     global cylinder
