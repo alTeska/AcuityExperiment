90d058c7e535ad3d622e9fa1fdebb76d94e2d2b7
Aleksandra Teska <aleksandra.teska@gmail.com>
2018-04-14 00:41:31 +0200

    sinplot update

--- scripts/sinplot.py
+++ scripts/sinplot.py
@@ -2,11 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cfg
 
-v = np.linspace(0, np.pi * cfg.SCALE, num=cfg.SIZE)
-m = np.repeat(np.sin(v)[np.newaxis, :], cfg.SIZE, axis=0)
+m = np.array([np.sin(np.linspace(-cfg.SCALE, cfg.SCALE, cfg.SIZE))]*cfg.SIZE)
 
 fig = plt.imshow(m, cmap='gray')
 plt.axis('off')
 fig.axes.get_xaxis().set_visible(False)
 fig.axes.get_yaxis().set_visible(False)
-plt.savefig(cfg.CYLINDER_TEXTURE, bbox_inches='tight', pad_inches=0, dpi=cfg.DPI, transparent=True)
+plt.savefig('scripts/assets/sin.png', bbox_inches='tight', pad_inches=0, dpi=cfg.DPI, transparent=True)
